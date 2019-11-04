bfd15e855b8d40f0bb24c11acf02fed9bc499b26
Filip Skokan <panva.ip@gmail.com>
2019-11-04 16:42:05 +0100

    fix: use sha512 for Ed25519 and shake256 for Ed448

--- test/generate.js
+++ test/generate.js
@@ -1,17 +1,25 @@
 const assert = require('assert');
+const crypto = require('crypto');
+
 const oidcTokenHash = require('..');
+
+const shake256 = crypto.getHashes().includes('shake256');
 
 /* eslint-disable max-len */
 
-assert.equal('x7vk7f6BvQj0jQHYFIk4ag', oidcTokenHash.generate('YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw'));
-assert.equal('x7vk7f6BvQj0jQHYFIk4ag', oidcTokenHash.generate('YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw', 'sha256'));
 assert.equal('x7vk7f6BvQj0jQHYFIk4ag', oidcTokenHash.generate('YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw', 'RS256'));
+assert.equal('x7vk7f6BvQj0jQHYFIk4ag', oidcTokenHash.generate('YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw', 'ES256'));
+assert.equal('x7vk7f6BvQj0jQHYFIk4ag', oidcTokenHash.generate('YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw', 'ES256K'));
 assert.equal('x7vk7f6BvQj0jQHYFIk4ag', oidcTokenHash.generate('YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw', 'HS256'));
 
-assert.equal('rEF91trffQGMQVyqcnZvwddyQGLs6GTF', oidcTokenHash.generate('NjlkNjk2ZGUtZWQ1YS00MmQ3LWExOGUtMjNiYTY2MDU4YjJhYmJls2NqVk9DX1z2vDl4MZMeF2XLfMwiXWmRYzUL0t_IY1nWFp6Moeu2GVmBoT_d0Acl4upDyfdGRjeR-ZIsUQ', 'sha384'));
 assert.equal('rEF91trffQGMQVyqcnZvwddyQGLs6GTF', oidcTokenHash.generate('NjlkNjk2ZGUtZWQ1YS00MmQ3LWExOGUtMjNiYTY2MDU4YjJhYmJls2NqVk9DX1z2vDl4MZMeF2XLfMwiXWmRYzUL0t_IY1nWFp6Moeu2GVmBoT_d0Acl4upDyfdGRjeR-ZIsUQ', 'RS384'));
 assert.equal('rEF91trffQGMQVyqcnZvwddyQGLs6GTF', oidcTokenHash.generate('NjlkNjk2ZGUtZWQ1YS00MmQ3LWExOGUtMjNiYTY2MDU4YjJhYmJls2NqVk9DX1z2vDl4MZMeF2XLfMwiXWmRYzUL0t_IY1nWFp6Moeu2GVmBoT_d0Acl4upDyfdGRjeR-ZIsUQ', 'HS384'));
 
-assert.equal('HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM', oidcTokenHash.generate('N2IwZmM5Y2YtZWJiYS00ZjA5LTkyZTktZTc0MjY5NDlmZDUwv7m2UfVNLyXYhcEicgYZ5LsQbZ7huJNibUjqAmPAWhyyxWRgXdZp4iTl2lE2ezdC3W-x93gkIg00rNok1MYgqA', 'sha512'));
 assert.equal('HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM', oidcTokenHash.generate('N2IwZmM5Y2YtZWJiYS00ZjA5LTkyZTktZTc0MjY5NDlmZDUwv7m2UfVNLyXYhcEicgYZ5LsQbZ7huJNibUjqAmPAWhyyxWRgXdZp4iTl2lE2ezdC3W-x93gkIg00rNok1MYgqA', 'RS512'));
 assert.equal('HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM', oidcTokenHash.generate('N2IwZmM5Y2YtZWJiYS00ZjA5LTkyZTktZTc0MjY5NDlmZDUwv7m2UfVNLyXYhcEicgYZ5LsQbZ7huJNibUjqAmPAWhyyxWRgXdZp4iTl2lE2ezdC3W-x93gkIg00rNok1MYgqA', 'HS512'));
+
+assert.equal('HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM', oidcTokenHash.generate('N2IwZmM5Y2YtZWJiYS00ZjA5LTkyZTktZTc0MjY5NDlmZDUwv7m2UfVNLyXYhcEicgYZ5LsQbZ7huJNibUjqAmPAWhyyxWRgXdZp4iTl2lE2ezdC3W-x93gkIg00rNok1MYgqA', 'EdDSA', 'Ed25519'));
+
+if (shake256) {
+  assert.equal('wvAJtH9mg9RPIhRyZ7Ji-w', oidcTokenHash.generate('N2IwZmM5Y2YtZWJiYS00ZjA5LTkyZTktZTc0MjY5NDlmZDUwv7m2UfVNLyXYhcEicgYZ5LsQbZ7huJNibUjqAmPAWhyyxWRgXdZp4iTl2lE2ezdC3W-x93gkIg00rNok1MYgqA', 'EdDSA', 'Ed448'));
+}
--- test/validate.js
+++ test/validate.js
@@ -1,90 +1,165 @@
 const assert = require('assert');
+const crypto = require('crypto');
+
 const oidcTokenHash = require('..');
+
+const shake256 = crypto.getHashes().includes('shake256');
 
 /* eslint-disable max-len, space-in-parens */
 
+assert.throws(() => oidcTokenHash.validate(
+  { claim: '', source: 'access_token' }
+), { name: /TypeError/, message: 'names.claim must be a non-empty string' });
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: '' }
+), { name: /TypeError/, message: 'names.source must be a non-empty string' });
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 0, source: 'access_token' }
+), { name: /TypeError/, message: 'names.claim must be a non-empty string' });
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 0 }
+), { name: /TypeError/, message: 'names.source must be a non-empty string' });
+
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
+  'x7vk7f6BvQj0jQHYFI',
+  'YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw',
+  'madeupalg'
+), { name: /AssertionError/, message: 'at_hash could not be validated (unrecognized or invalid JWS algorithm provided)' });
+
+
 // non correct lengths
-assert(!oidcTokenHash(
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   'x7vk7f6BvQj0jQHYFI',
   'YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw',
   'RS256'
-));
-assert(!oidcTokenHash(
+), { name: /AssertionError/, message: 'at_hash mismatch, expected x7vk7f6BvQj0jQHYFIk4ag, got: x7vk7f6BvQj0jQHYFI' });
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   'x7vk7fx7vk7f6BvQj0jQHYFI',
   'YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw',
   'RS256'
-));
+), { name: /AssertionError/, message: 'at_hash mismatch, expected x7vk7f6BvQj0jQHYFIk4ag, got: x7vk7fx7vk7f6BvQj0jQHYFI' });
 
-assert(!oidcTokenHash('', '', 'none'));
-assert(!oidcTokenHash(null, null, 'none'));
-assert(!oidcTokenHash(null, undefined, 'none'));
-assert(!oidcTokenHash(undefined, null, 'none'));
-assert(!oidcTokenHash(undefined, undefined, 'none'));
+assert.throws(() => oidcTokenHash.validate({ claim: 'at_hash', source: 'access_token' }, '', 'foo', 'HS256'), { name: /AssertionError/, message: 'at_hash must be a non-empty string' });
+assert.throws(() => oidcTokenHash.validate({ claim: 'at_hash', source: 'access_token' }, 'foo', '', 'HS256'), { name: /AssertionError/, message: 'access_token must be a non-empty string' });
+assert.throws(() => oidcTokenHash.validate({ claim: 'at_hash', source: 'access_token' }, null, 'foo', 'HS256'), { name: /AssertionError/, message: 'at_hash must be a non-empty string' });
+assert.throws(() => oidcTokenHash.validate({ claim: 'at_hash', source: 'access_token' }, 'foo', null, 'HS256'), { name: /AssertionError/, message: 'access_token must be a non-empty string' });
+assert.throws(() => oidcTokenHash.validate({ claim: 'at_hash', source: 'access_token' }, undefined, 'foo', 'HS256'), { name: /AssertionError/, message: 'at_hash must be a non-empty string' });
+assert.throws(() => oidcTokenHash.validate({ claim: 'at_hash', source: 'access_token' }, 'foo', undefined, 'HS256'), { name: /AssertionError/, message: 'access_token must be a non-empty string' });
+assert.throws(() => oidcTokenHash.validate({ claim: 'at_hash', source: 'access_token' }, 0, 'foo', 'HS256'), { name: /AssertionError/, message: 'at_hash must be a non-empty string' });
+assert.throws(() => oidcTokenHash.validate({ claim: 'at_hash', source: 'access_token' }, 'foo', 0, 'HS256'), { name: /AssertionError/, message: 'access_token must be a non-empty string' });
 
 // 256 lengths
-assert( oidcTokenHash(
+assert(!oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   'x7vk7f6BvQj0jQHYFIk4ag',
   'YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw',
   'HS256'
 ));
-assert(!oidcTokenHash(
+assert(!oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
+  'x7vk7f6BvQj0jQHYFIk4ag',
+  'YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw',
+  'ES256'
+));
+assert(!oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
+  'x7vk7f6BvQj0jQHYFIk4ag',
+  'YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw',
+  'ES256K'
+));
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   'x7vk7f6BvQj0jQHYFIk4ag',
   '--JiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw',
   'HS256'
-));
-assert(!oidcTokenHash(
+), { name: /AssertionError/, message: 'at_hash mismatch, expected n7fjT8SGm5ThW4f3zFRYBg, got: x7vk7f6BvQj0jQHYFIk4ag' });
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   '--vk7f6BvQj0jQHYFIk4--',
   'YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw',
   'HS256'
-));
+), { name: /AssertionError/, message: 'at_hash mismatch, expected x7vk7f6BvQj0jQHYFIk4ag, got: --vk7f6BvQj0jQHYFIk4--' });
 
 // 384 lengths
-assert( oidcTokenHash(
+assert(!oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   'rEF91trffQGMQVyqcnZvwddyQGLs6GTF',
   'NjlkNjk2ZGUtZWQ1YS00MmQ3LWExOGUtMjNiYTY2MDU4YjJhYmJls2NqVk9DX1z2vDl4MZMeF2XLfMwiXWmRYzUL0t_IY1nWFp6Moeu2GVmBoT_d0Acl4upDyfdGRjeR-ZIsUQ',
   'RS384'
 ));
-assert(!oidcTokenHash(
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   'rEF91trffQGMQVyqcnZvwddyQGLs6GTF',
   '--lkNjk2ZGUtZWQ1YS00MmQ3LWExOGUtMjNiYTY2MDU4YjJhYmJls2NqVk9DX1z2vDl4MZMeF2XLfMwiXWmRYzUL0t_IY1nWFp6Moeu2GVmBoT_d0Acl4upDyfdGRjeR-ZIsUQ',
   'RS384'
-));
-assert(!oidcTokenHash(
+), { name: /AssertionError/, message: 'at_hash mismatch, expected AbnGYqUea5QYS505GJcm97screFkcCGQ, got: rEF91trffQGMQVyqcnZvwddyQGLs6GTF' });
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   '--F91trffQGMQVyqcnZvwddyQGLs6G--',
   'NjlkNjk2ZGUtZWQ1YS00MmQ3LWExOGUtMjNiYTY2MDU4YjJhYmJls2NqVk9DX1z2vDl4MZMeF2XLfMwiXWmRYzUL0t_IY1nWFp6Moeu2GVmBoT_d0Acl4upDyfdGRjeR-ZIsUQ',
   'RS384'
-));
+), { name: /AssertionError/, message: 'at_hash mismatch, expected rEF91trffQGMQVyqcnZvwddyQGLs6GTF, got: --F91trffQGMQVyqcnZvwddyQGLs6G--' });
 
 // 512 lengths
-assert( oidcTokenHash(
+assert(!oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   'HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM',
   'N2IwZmM5Y2YtZWJiYS00ZjA5LTkyZTktZTc0MjY5NDlmZDUwv7m2UfVNLyXYhcEicgYZ5LsQbZ7huJNibUjqAmPAWhyyxWRgXdZp4iTl2lE2ezdC3W-x93gkIg00rNok1MYgqA',
   'ES512'
 ));
-assert(!oidcTokenHash(
+assert(!oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
+  'HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM',
+  'N2IwZmM5Y2YtZWJiYS00ZjA5LTkyZTktZTc0MjY5NDlmZDUwv7m2UfVNLyXYhcEicgYZ5LsQbZ7huJNibUjqAmPAWhyyxWRgXdZp4iTl2lE2ezdC3W-x93gkIg00rNok1MYgqA',
+  'EdDSA',
+  'Ed25519'
+));
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
+  'HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM',
+  'N2IwZmM5Y2YtZWJiYS00ZjA5LTkyZTktZTc0MjY5NDlmZDUwv7m2UfVNLyXYhcEicgYZ5LsQbZ7huJNibUjqAmPAWhyyxWRgXdZp4iTl2lE2ezdC3W-x93gkIg00rNok1MYgqA',
+  'EdDSA',
+  'edfoo'
+), { name: /AssertionError/, message: 'at_hash could not be validated (unrecognized or invalid EdDSA curve provided)' });
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
+  'HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM',
+  'N2IwZmM5Y2YtZWJiYS00ZjA5LTkyZTktZTc0MjY5NDlmZDUwv7m2UfVNLyXYhcEicgYZ5LsQbZ7huJNibUjqAmPAWhyyxWRgXdZp4iTl2lE2ezdC3W-x93gkIg00rNok1MYgqA',
+  'EdDSA',
+  'Ed448'
+), { name: /AssertionError/, message: shake256 ? 'at_hash mismatch, expected wvAJtH9mg9RPIhRyZ7Ji-w, got: HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM' : 'at_hash could not be validated (Digest method not supported)' });
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   'HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM',
   '--IwZmM5Y2YtZWJiYS00ZjA5LTkyZTktZTc0MjY5NDlmZDUwv7m2UfVNLyXYhcEicgYZ5LsQbZ7huJNibUjqAmPAWhyyxWRgXdZp4iTl2lE2ezdC3W-x93gkIg00rNok1MYgqA',
   'ES512'
-));
-assert(!oidcTokenHash(
+), { name: /AssertionError/, message: 'at_hash mismatch, expected IvHMT4I56xNW8OVJW1_gB4UOPLsiX6iPbX3eSBfNfXo, got: HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM' });
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   '--IOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1--',
   'N2IwZmM5Y2YtZWJiYS00ZjA5LTkyZTktZTc0MjY5NDlmZDUwv7m2UfVNLyXYhcEicgYZ5LsQbZ7huJNibUjqAmPAWhyyxWRgXdZp4iTl2lE2ezdC3W-x93gkIg00rNok1MYgqA',
   'ES512'
-));
+), { name: /AssertionError/, message: 'at_hash mismatch, expected HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM, got: --IOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1--' });
 
 // mismatch between header alg and the length of the hash
-assert(!oidcTokenHash(
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   'x7vk7f6BvQj0jQHYFIk4ag',
   'YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL-ar_qw5jl3lthwpMjm283aVMQXDmoqqqydDSqJfbhptzw8rUVwkuQbolw',
   'RS384'
-));
-assert(!oidcTokenHash(
+), { name: /AssertionError/, message: 'at_hash mismatch, expected ups_76_7CCye_J1WIyGHKVG7AAs2olYm, got: x7vk7f6BvQj0jQHYFIk4ag' });
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   'rEF91trffQGMQVyqcnZvwddyQGLs6GTF',
   'NjlkNjk2ZGUtZWQ1YS00MmQ3LWExOGUtMjNiYTY2MDU4YjJhYmJls2NqVk9DX1z2vDl4MZMeF2XLfMwiXWmRYzUL0t_IY1nWFp6Moeu2GVmBoT_d0Acl4upDyfdGRjeR-ZIsUQ',
   'RS256'
-));
-assert(!oidcTokenHash(
+), { name: /AssertionError/, message: 'at_hash mismatch, expected uLvLJzpaRoJQiSGRDpq2sg, got: rEF91trffQGMQVyqcnZvwddyQGLs6GTF' });
+assert.throws(() => oidcTokenHash.validate(
+  { claim: 'at_hash', source: 'access_token' },
   'HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM',
   'N2IwZmM5Y2YtZWJiYS00ZjA5LTkyZTktZTc0MjY5NDlmZDUwv7m2UfVNLyXYhcEicgYZ5LsQbZ7huJNibUjqAmPAWhyyxWRgXdZp4iTl2lE2ezdC3W-x93gkIg00rNok1MYgqA',
   'RS256'
-));
+), { name: /AssertionError/, message: 'at_hash mismatch, expected a_8m332a5VeA9DZTBIa_Cg, got: HgIOjpEKMhvtwzZvjUdUmMYayc0gOvaxkZEsautS1KM' });
